609d1309d12acbdcef34142986b293906edb59c4
magnarox <magnarox06@gmail.com>
2019-10-22 10:12:03 +0200

    FIX notebook connexion

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -12,10 +12,19 @@ c = get_config()
 
 # Spawn single-user servers as Docker containers
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+
+c.JupyterHub.hub_ip = os.environ['HUB_IP']
+
 # Spawn containers from this image
 c.DockerSpawner.image = os.environ['DOCKER_NOTEBOOK_IMAGE']
 
-c.DockerSpawner.remove_containers = True
+# JupyterHub requires a single-user instance of the Notebook server, so we
+# default to using the `start-singleuser.sh` script included in the
+# jupyter/docker-stacks *-notebook images as the Docker run command when
+# spawning containers.  Optionally, you can override the Docker run command
+# using the DOCKER_SPAWN_CMD environment variable.
+#spawn_cmd = os.environ.get('DOCKER_SPAWN_CMD', "start-singleuser.sh")
+#c.DockerSpawner.extra_create_kwargs.update({ 'command': spawn_cmd })
 
 network_name = os.environ['DOCKER_NETWORK_NAME']
 c.DockerSpawner.use_internal_ip = True
@@ -23,5 +32,19 @@ c.DockerSpawner.network_name = network_name
 # Pass the network name as argument to spawned containers
 c.DockerSpawner.extra_host_config = { 'network_mode': network_name }
 
-c.JupyterHub.hub_ip = os.environ['HUB_IP']
-c.JupyterHub.hub_port = 8080
+notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR')
+c.DockerSpawner.notebook_dir = notebook_dir
+
+# Mount the real user's Docker volume on the host to the notebook user's
+# notebook directory in the container
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+
+c.DockerSpawner.hub_connect_ip = os.environ['HUB_IP']
+
+# Remove containers once they are stopped
+c.DockerSpawner.remove_containers = True
+c.DockerSpawner.remove = True
+# For debugging arguments passed to spawned containers
+c.DockerSpawner.debug = True
+
+#c.JupyterHub.hub_port = 8080
